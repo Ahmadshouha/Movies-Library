1b906a8395bbaf6b3e541693a239efd8118ad28e
Ahmadshouha <shouha96@outlook.com>
2022-02-12 20:25:28 +0200

    added some code

--- server.js
+++ server.js
@@ -1,44 +1,107 @@
 'use strict';
 
-// npm init -y 
-// npm i express
-// npm i cors 
-// npm i express cors
+require('dotenv').config();
 
 const data = require('./data.json');
 
-// use the packages ?
 const express = require('express');
 const cors = require('cors');
-
-// creating a server 
+const axios = require('axios');
 const server = express();
+const port= process.env.PORT
+
 server.use(cors());
-server.get('/', handelMovie )
-server.get('/favorite', handelGet ) //localhost:3000
+server.use (express.json);
 
+server.get('/', handelMovie)
+server.get('/favorite', handelGet) //Welcome to Favorite Page
+server.get('/trending',handelTrending)
+server.get('/search',handelSearching)
+server.get('/rout1',handelRout1)
+server.get('/rout2',handelRout2)
+server.use('', handleError) // 404 Error
+server.use (handleError500)
 
-function Movie(title,poster_path,overview){
-    this.title= title;
-    this.poster_path =poster_path;
-    this.overview=overview;
- }
+let url=`https://api.themoviedb.org/3/trending/all/week?api_key=${process.env.APIKEY}`
 
- 
-function handelGet(request,response){
-   return response.status(200).send("Welcome to Favorite Page")
+function Movie(id,title,release_date, poster_path, overview) {
+    this.id=id;
+    this.title = title;
+    this.release_date=release_date;
+    this.poster_path = poster_path;
+    this.overview = overview;
+   
+    
 }
 
 
+/ **   handelMovie ** /
+function handelMovie(req, res) {
 
-server.listen(3000,()=>{
+    let obj = new Movie(data.title, data.poster_path, data.overview);
+    return res.status(200).json(obj);
+}
+
+
+//**    handelGet        ** /
+function handelGet(request, response) {
+    return response.status(200).send("Welcome to Favorite Page")
+}
+
+function handelTrending(request, response) {
+   axios.get(url).then((access)=>{let movieData=access.data.results
+let movieC=movieData.map((access2)=>{return new Movie(access2.id,access2.title,access2.release_date,access2.poster_path,access2.overview);
+     })
+response.status(200).json(movieC)
+   }).catch((err) => {
+    console.log("Error");
+   })
+}
+
+let movie = "Riverdance: The Animated Adventure";
+
+function handelSearching(request, response) {
+    let url2=`https://api.themoviedb.org/3/search/movie?api_key=${process.env.APIKEY}&query=${movie} `;
+    axios.get(url2).then((access)=>{let movieData=access.data.results
+ let movieC=movieData.map((access2)=>{return new Movie(access2.id,access2.title,access2.release_date,access2.poster_path,access2.overview);
+      })
+ response.status(200).json(movieC)
+    }).catch((err) => {
+     console.log("Error");
+    })
+ }
+ function handelSearching(request, response) {
+ let url3 = `https://api.themoviedb.org/3/genre/movie/list?api_key=${process.env.APIKEY}`;
+ axios.get(url3)
+ .then((access3) => {
+     let result = access3.data;
+     res.status(200).json(result);
+     console.log(result);
+ }).catch((err) => {
+     console.log("Error");
+ })}
+ 
+
+
+
+
+/**        handleError404          **/
+function handleError(request, response) {
+    response.status(404).send('page not found error 404')
+}
+
+
+/**       handleError500          **/
+function handleError500(error,request, response) {
+    const err = {
+        "status": 500,
+        "responseText": "Sorry, something went wrong"
+    }
+    response.status(500).send(err);
+
+}
+
+
+server.listen(3000, () => {
     console.log("my server is listining to port 3000");
 })
-function handelMovie(req,res){
-    let movies = [];
-    data.data.forEach(val=>{
-        let obj = new Movie(val.title, val.poster_path, val.overview);
-        movies.push(obj);
-    })
-    return res.status(200).json(movies);
-}
