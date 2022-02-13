3cf78979bd435cedf0c8b7d51d448bde2eca488d
Ahmadshouha <shouha96@outlook.com>
2022-02-13 10:43:26 +0200

    added some code

---
+++ schema.sql
@@ -0,0 +1,11 @@
+
+DROP TABLE IF EXISTS ACTIONMOVIE;
+
+CREATE TABLE IF NOT EXISTS ACTIONMOVIE (
+   id SERIAL PRIMARY KEY,
+    title VARCHAR(255),
+    release_date VARCHAR(255),
+    poster_path VARCHAR(255),
+    overview VARCHAR(3000)
+
+);
